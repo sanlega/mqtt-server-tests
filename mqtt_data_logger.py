41a5e727db4d4ff7dc2dd91e5b0cebce9c6e9866
Sanlega <slegaris@student.42madrid.com>
2023-09-05 05:07:00 +0200

    Syntax correction from previous commit about subtopics

--- mqtt_data_logger.py
+++ mqtt_data_logger.py
@@ -17,7 +17,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with Code: " + str(rc))
     client.subscribe("test/#")  # Replace with your topic name if it's followed by # it will listen for all subtopics after it
 
-### An example of topic might be "main/temps/#/celsius" that will give us the celsius temps of all the subtopics after "/temps"
+### An example of topic might be "main/temps/#/celsius" that will give us the celsius temps of all the subtopics after "main/temps/..."
 
 def on_message(client, userdata, msg):
     print("Message received: " + msg.payload.decode("utf-8"))
